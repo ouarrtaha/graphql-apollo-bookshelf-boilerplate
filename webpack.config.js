4ac69d1b52c2a05c32a87a7b7e62a0b7fd86cddc
taha_mac <tahaouarrak@gmail.com>
2019-01-01 05:07:53 +0100

    Add babel-loader and babel-polyfill

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+
+module.exports = {
+    entry: [
+        './src/index.js'
+    ],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "js/bundle.js",
+    },
+    mode: process.env.NODE_ENV || 'development', //use webpack --mode development
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+
+}
