868d1b3fe5563e141be275801b635d6f3521507e
taha_mac <tahaouarrak@gmail.com>
2019-01-01 18:25:08 +0100

    Refactor types

--- src/graphql/resolvers/post.js
+++ src/graphql/resolvers/post.js
@@ -1,24 +1,24 @@
 export default {
   Query: {
-    getArticles (obj, args, context, info) {
+    getPosts (parent, args, context, info) {
       return [
         {
           id: 1,
-          title: 'Article1',
+          title: 'Post1',
           content: 'Sample Content',
           negatifVotes: 3,
           positifVotes: 5
         },
         {
           id: 2,
-          title: 'Article2',
+          title: 'Post2',
           content: 'Sample Content',
           negatifVotes: 4,
           positifVotes: 6
         },
         {
           id: 3,
-          title: 'Article3',
+          title: 'Post3',
           content: 'Sample Content',
           negatifVotes: 6,
           positifVotes: 2
