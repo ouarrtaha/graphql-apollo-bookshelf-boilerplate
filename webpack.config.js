2264672cb22e70a98dda212c854bc58773310676
taha_mac <tahaouarrak@gmail.com>
2019-01-01 05:10:09 +0100

    Add index.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     ],
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: "js/bundle.js",
+        filename: "bundle.js",
     },
     mode: process.env.NODE_ENV || 'development', //use webpack --mode development
     devServer: {
