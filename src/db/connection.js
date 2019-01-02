a5bd6c97e7edca1105f09ac0c25929e16828099f
taha_mac <tahaouarrak@gmail.com>
2019-01-02 02:23:19 +0100

    Little refactoring

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,9 +1,9 @@
-import bk from 'bookshelf';
+import bs from 'bookshelf';
 import knex from 'knex';
 import knexConfig from './knexfile';
 
 export const config = knexConfig[process.env.NODE_ENV];
-export const bookshelf = bk(knex(config));
+export const bookshelf = bs(knex(config));
 export const db = knex(config);
 
 // Export constructor and config object
